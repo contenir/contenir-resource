d38f0d4e8ae14a5872006671ce69b4e504d9f94f
Simon Mundy <simon.mundy@peptolab.com>
2023-09-11 11:13:19 +1000

    Added new View Helper to generate URLs from Resource instances

---
+++ src/View/Helper/ResourceUrl.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Contenir\Resource\View\Helper;
+
+use Contenir\Resource\Model\Entity\BaseResourceEntity;
+use Laminas\View\Helper\AbstractHelper;
+
+class ResourceUrl extends AbstractHelper
+{
+    public function __invoke($resourceId, $resourceUrl, $linkTarget = null)
+    {
+        $url = null;
+
+        if ($resourceId instanceof BaseResourceEntity) {
+            $url = $this->view->Url($resourceId->getRouteId());
+        } elseif ($resourceId) {
+            $resource = $this->view->Resource($resourceId);
+            if ($resource) {
+                $url = $this->view->Url($resource->getRouteId());
+            }
+        } elseif ($resourceUrl) {
+            $url        = $this->view->UrlFormat($resourceUrl);
+            $linkTarget = '_blank';
+        }
+
+        return [$url, $linkTarget];
+    }
+}
